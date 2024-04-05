56b19c4b87c0d186e6191da5e824540657d98236
Emily Ryznar <emily.ryznar@nmfs.local>
2024-04-05 14:00:51 -0800

    Fitting gamm models by knots, era, and sst smooth

--- Scripts/ts.exploration.R
+++ Scripts/ts.exploration.R
@@ -33,83 +33,70 @@ source("./Scripts/ts.processing.R")
         dplyr::select(!region) -> goa.r0.sst
       
       # Make function to streamline fitting
-        fit.models <- function(dat){
+      fit.models <- function(dat){
        
         for(ii in 1:length(unique(dat$TS))){
           # filter data by TS
           dat %>%
-            filter(TS == unique(dat$TS)[ii]) -> TS.dat
+            mutate(era = case_when((Year <=1989) ~ "early",
+                                    (Year >1989) ~ "late")) %>%
+            filter(TS == unique(dat$TS)[ii]) %>%
+            group_by(TS, era) %>%
+            mutate(N = n()) %>%
+            ungroup()-> TS.dat
           
-          # fit gams, record pval and AIC
-          gam.1 <- gam(log.recruitment ~ s(sst, k = 3), data= TS.dat, method = "REML")
-          gam.2 <- gam(log.recruitment ~ s(sst.2, k = 3), data= TS.dat, method = "REML")
-          gam.3 <- gam(log.recruitment ~ s(sst.3, k = 3), data= TS.dat, method = "REML")
+          knts <- c(3, 4)
           
-          p.gam.1 <- signif(summary(gam.1)$s.table[,4], 2)
-          p.gam.2 <- signif(summary(gam.2)$s.table[,4],2)
-          p.gam.3 <- signif(summary(gam.3)$s.table[,4],2)
-          
-          AIC.gam.1 <- AICc(gam.1)
-          AIC.gam.2 <- AICc(gam.2)
-          AIC.gam.3 <- AICc(gam.3)
-          
-          # fit gls, record pval and AIC
-          gls.1 <- gls(log.recruitment ~ sst, data = TS.dat, correlation = corAR1())
-          gls.2 <- gls(log.recruitment ~ sst.2, data = TS.dat, correlation = corAR1())
-          gls.3 <- gls(log.recruitment ~ sst.3, data = TS.dat, correlation = corAR1())
-          
-          p.gls.1 <- signif(summary(gls.1)$tTable[2,4], 2)
-          p.gls.2 <- signif(summary(gls.2)$tTable[2,4], 2)
-          p.gls.3 <- signif(summary(gls.3)$tTable[2,4], 2)
-          
-          AIC.gls.1 <- AICc(gls.1)
-          AIC.gls.2 <- AICc(gls.2)
-          AIC.gls.3 <- AICc(gls.3)
-          
-          # Build summary table
-          model.out <- rbind(model.out, data.frame(TS = unique(dat$TS)[ii],
-                                                   Model = c("gam.1", "gam.2", "gam.3", "gls.1", "gls.2", "gls.3"),
-                                                   p_val = c(p.gam.1, p.gam.2, p.gam.3, p.gls.1, p.gls.2, p.gls.3),
-                                                   AIC = c(AIC.gam.1, AIC.gam.2, AIC.gam.3, AIC.gls.1, AIC.gls.2, AIC.gls.3)))
-          
-        }
+          for(kk in 1:length(knts)){
+            for(jj in 1:length(unique(TS.dat$era))){
+              
+              # fit gams, record pval and AIC
+              gam.1 <- gamm(log.recruitment ~ s(sst, k = knts[kk]), 
+                           data= TS.dat %>% filter(era == unique(TS.dat$era)[jj]), correlation = corAR1())
+              gam.2 <- gamm(log.recruitment ~ s(sst.2, k = knts[kk]), 
+                            data= TS.dat %>% filter(era == unique(TS.dat$era)[jj]), correlation = corAR1())
+              gam.3 <- gamm(log.recruitment ~ s(sst.3, k = knts[kk]), 
+                            data= TS.dat %>% filter(era == unique(TS.dat$era)[jj]), correlation = corAR1())
+              
+              p.gam.1 <- signif(summary(gam.1$gam)$s.table[,4],2)
+              p.gam.2 <- signif(summary(gam.2$gam)$s.table[,4],2)
+              p.gam.3 <- signif(summary(gam.3$gam)$s.table[,4],2)
+              
+              AIC.gam.1 <- AICc(gam.1)
+              AIC.gam.2 <- AICc(gam.2)
+              AIC.gam.3 <- AICc(gam.3)
+  
+              
+              # Build summary table
+              model.out <- rbind(model.out, data.frame(TS = unique(dat$TS)[ii],
+                                                       knots = knts[kk],
+                                                       era = unique(TS.dat$era)[jj],
+                                                       #Model = c("gam.1", "gam.2", "gam.3"),
+                                                       smooth = c("unsmoothed", "2-year", "3-year"),
+                                                       p_val = c(p.gam.1, p.gam.2, p.gam.3),
+                                                       AIC = c(AIC.gam.1, AIC.gam.2, AIC.gam.3),
+                                                       N = TS.dat %>% 
+                                                            filter(era ==unique(TS.dat$era)[jj]) %>%
+                                                         dplyr::select(N) %>%
+                                                         pull() %>%
+                                                         unique()))
+            } # close era loop
+          } # close knot loop
+        } # close timeseries loop
+        
+        # Label best models by timeseries
+        model.out %>%
+          group_by(TS, era) %>%
+          mutate(BEST = ifelse(AIC == min(AIC), "Y", "N")) -> model.out
         
         return(model.out)
       }
       
+    
       # Fit models for BSAI
         model.out <- data.frame()
         
         fit.models(bsai.r0.sst) -> bsai.r0.out
-        
-        bsai.r0.out %>%
-          group_by(Model) %>%
-          reframe(mean.AIC = mean(AIC)) -> bsai.sum
-        
-        as.data.frame(bsai.r0.out) %>%
-          filter(grepl("gam", Model) == TRUE) -> out
-        
-        ggplot(out, mapping = aes(TS, abs(AIC), fill = Model))+
-          geom_bar(stat = "identity", position = "dodge")+
-          theme_bw() +
-          scale_fill_manual(values = c("#0E3F5C","#579C97","#D1FBD4"), 
-                            labels = c("Unsmoothed", "2-year mean", "3-year mean"),
-                            name = "")+
-          geom_text(out, mapping = aes(x = TS, y = abs(AIC)+2), label = ifelse(out$p_val < 0.05, "*", ""),
-                    position = position_dodge(width = 1), size = 8)+
-          scale_x_discrete(breaks = names(r0.labs.bsai), labels = function(x)
-            str_wrap(r0.labs.bsai, width = 17))+
-          ylab("abs(AIC)")+
-          ggtitle("BSAI SST vs. recruitment AIC")+
-          theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
-                axis.title.x = element_blank(),
-                axis.text = element_text(size = 14),
-                axis.title = element_text(size = 16),
-                legend.position = "bottom",
-                legend.text =  element_text(size = 14),
-                title = element_text(size = 16))
-        
-        ggsave("./Figures/bsai.r0.SST.png", width = 11, height = 8.5, units = "in")
         
       
       # Fit models for GOA
@@ -117,41 +104,11 @@ source("./Scripts/ts.processing.R")
         
         fit.models(dat = goa.r0.sst) -> goa.r0.out
         
-          goa.r0.out %>%
-            group_by(Model) %>%
-            reframe(mean.AIC = mean(AIC)) -> goa.sum
-          
-          as.data.frame(goa.r0.out) %>%
-            filter(grepl("gam", Model) == TRUE) -> out
-          
-          ggplot(out, mapping = aes(TS, abs(AIC), fill = Model))+
-            geom_bar(stat = "identity", position = "dodge")+
-            theme_bw() +
-            scale_fill_manual(values = c("#0E3F5C","#579C97","#D1FBD4"), 
-                              labels = c("Unsmoothed", "2-year mean", "3-year mean"),
-                              name = "")+
-            geom_text(out, mapping = aes(x = TS, y = abs(AIC)+2), label = ifelse(out$p_val < 0.05, "*", ""),
-                      position = position_dodge(width = 1), size = 8)+
-            scale_x_discrete(breaks = names(r0.labs.goa), labels = function(x)
-              str_wrap(r0.labs.goa, width = 17))+
-            ylab("abs(AIC)")+
-            ggtitle("GOA SST vs. recruitment AIC")+
-            theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
-                  axis.title.x = element_blank(),
-                  axis.text = element_text(size = 14),
-                  axis.title = element_text(size = 16),
-                  legend.position = "bottom",
-                  legend.text =  element_text(size = 14),
-                  title = element_text(size = 16))
-          
-          ggsave("./Figures/goa.r0.SST.png", width = 11, height = 8.5, units = "in")
+        
           
     
   # Calculate AR1 and SD for 15 year windows for biology/SST timeseries ----
-    # Specify 15-year windows
-    win.1 <- seq(min(years), min(years)+14, by= 1)
-    win.2 <- seq(max(win.1)+1, max(win.1)+15)
-    
+  
     # Create function to calculate values
     sum.fun <- function(dat, data.type){
       
