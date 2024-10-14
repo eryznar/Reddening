9a47f25cf1a120c0d35a66c6d6ea8f8b77c4c587
Emily Ryznar <emily.ryznar@nmfs.local>
2024-10-14 11:25:12 -0800

    script update

--- Scripts/ERA5_processing_LMEs.R
+++ Scripts/ERA5_processing_LMEs.R
@@ -11,6 +11,12 @@ north40 <- c(1, 2, 8, 9, 18, 19, 20, 21, 22, 23, 24, 51, 52, 53, 54, 55, 56, 57,
 subset(lme, lme$ARCTIC=="Arctic") -> arcticlme
 subset(lme, lme$LME_NUMBER %in% north40) ->  north40lme
 
+unique(north40lme$LME_NAME)
+drop <- c("Laptev Sea", "Kara Sea", "Canadian Eastern Arctic - West Greenland", 
+          "Beaufort Sea", "Central Arctic", "East Siberian Sea")
+
+subset(north40lme, !north40lme$LME_NAME %in% drop) -> north40lme
+
 
 unique(north40lme$LME_NAME) -> lme_names
 unique(north40lme$LME_NUMBER) -> lme_num
@@ -29,12 +35,20 @@ for(ii in 1:length(lme_names)){
       as.data.frame() -> late
     
     cbind(early, mid, late) %>%
-      na.omit() %>%
+      na.omit() -> tt
+    
+    tt[!duplicated(colnames(tt))] -> dat
+    
+    dat %>%
+      mutate(cell.mean = rowMeans(.)) %>%
       pivot_longer(., cols = 1:768, names_to = "time", values_to = "sst") %>%
-      mutate(year = str_sub(time, 2, 5),
-             LME = lme_names[ii]) %>%
-      group_by(year, LME) %>%
-      reframe(mean.sst = mean(sst-273.15)) -> df
+      mutate(anom = sst-cell.mean,
+             year = str_sub(time, 2, 5),
+             month = str_sub(time, 7, 8),
+             LME = lme_names[ii])  %>%
+      group_by(year, month, LME) %>%
+      reframe(mean.sst = mean(sst-273.15),
+              mean.anom = mean(anom)) -> df
     
     rbind(sst_df, df) -> sst_df
     
@@ -44,7 +58,18 @@ for(ii in 1:length(lme_names)){
 sst_df %>%
   right_join(., data.frame("LME" = lme_names, "LME_NUM"= lme_num), by = "LME") -> sst_df
 
+sst_df %>%
+  mutate(month = as.numeric(as.character(month)),
+         year = as.numeric(as.character(year)),
+         year = case_when((month %in% c(11, 12)) ~ (year +1),
+                          TRUE ~ year)) %>%
+  filter(month %in% c(11,12, 1, 2, 3)) %>%
+  group_by(year, LME, LME_NUM) %>%
+  reframe(mean.sst = mean(mean.sst),
+          mean.anom = mean(mean.anom)) -> sst_df2
+  
+
 
 # sst_df <- read.csv("./Output/lme.sst_df.csv") %>%
 #   mutate(mean.sst = mean.sst-273.15)
-write.csv(sst_df, "./Output/lme.sst_df.csv")
+write.csv(sst_df2, "./Output/lme.sst_df.csv")
